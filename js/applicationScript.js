711e36a49d4cbbdc8fa4b81b3df4ee6b226c1a76
CAEmario <CAEAddress@gmail.com>
2017-11-08 12:38:31 +0100

    MicroserviceCall edited by Mario Rosenstengel

--- js/applicationScript.js
+++ js/applicationScript.js
@@ -59,7 +59,7 @@ var nextQuiz = function(){
 
 //end variable declaration
 
-  client.sendRequest("GET", "videoquiz/getQuiz/", "", "", {}, false,
+  client.sendRequest("GET", "videoquiz/getQuiz/"+window.counter, "", "", {}, false,
   function(data, type) {
     console.log(data);
   },
